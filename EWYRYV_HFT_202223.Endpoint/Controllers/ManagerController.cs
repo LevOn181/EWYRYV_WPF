481bbbd5a2af491d4323576d0bebb93d3cdcf3d0
Levente181 <1222490@isep.ipp.pt>
2023-11-21 14:53:09 +0100

    SignalRHub added to ManagersController.

--- EWYRYV_HFT_202223.Endpoint/Controllers/ManagerController.cs
+++ EWYRYV_HFT_202223.Endpoint/Controllers/ManagerController.cs
@@ -1,6 +1,8 @@
-﻿using EWYRYV_HFT_202223.Logic;
+﻿using EWYRYV_HFT_202223.Endpoint.Services;
+using EWYRYV_HFT_202223.Logic;
 using EWYRYV_HFT_202223.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,9 +15,11 @@ namespace EWYRYV_HFT_202223.Endpoint.Controllers
     public class ManagerController : ControllerBase
     {
         IManagerLogic logic;
+        IHubContext<SignalRHub> hub;
 
         public ManagerController(IManagerLogic logic)
         {
+            this.hub = hub;
             this.logic = logic;
         }
 
@@ -35,18 +39,22 @@ namespace EWYRYV_HFT_202223.Endpoint.Controllers
         public void Create([FromBody] Manager value)
         {
             this.logic.Create(value);
+            this.hub.Clients.All.SendAsync("ManagerCreated", value);
         }
 
         [HttpPut]
         public void Update([FromBody] Manager value)
         {
             this.logic.Update(value);
+            this.hub.Clients.All.SendAsync("ManagerUpdated", value);
         }
 
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+            var ManagerToDelete = this.logic.Read(id);
             this.logic.Delete(id);
+            this.hub.Clients.All.SendAsync("ManagerDeleted", ManagerToDelete);
         }
     }
 }
